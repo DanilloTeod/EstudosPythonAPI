2a474433dc6a4cfc61544fb7fa5129c33547583f
Danillo Teodoro <danillo.felicio2004@gmail.com>
2026-09-20 12:01:41 -0300

    usando ORM do django

--- agenda/models.py
+++ agenda/models.py
@@ -1,19 +1,29 @@
 from django.db import models
 
+
+class Categoria(models.Model): # mapea os atributos dessa classe para o db
+    nome = models.CharField(max_length=256, unique=True)
+
 # Create your models here.
 # classes que representam os modelos ou entidades da nossa aplicacao ex: usuarios
+class Evento(models.Model):
+    #cria os elementos no db
+    nome = models.CharField(max_length=256) # não precisa ser unico, podemos ter dois eventos com mesmo nome sendo criado
+    categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True) # é uma chave estrangeira para outro modelo
+                                                                                   # caso seja deletado, setado como nulo
+    local = models.CharField(max_length=256, blank=True)
+    link = models.CharField(max_length=256, blank=True)
 
-class Evento:
-    def __init__(self, nome, categoria, local=None, link=None):
-        self.nome = nome
-        self.categoria = categoria
-        self.local = local
-        self.link = link
+    # def __init__(self, nome, categoria, local=None, link=None):
+    #     self.nome = nome
+    #     self.categoria = categoria
+    #     self.local = local
+    #     self.link = link
         
-aula_python = Evento("Aula de python", "backend", "Espirito santo")
-aula_js = Evento("Aula de JavaScript", "Fullstack", link="https://auladejs.com.br/")
+# aula_python = Evento("Aula de python", "backend", "Espirito santo")
+# aula_js = Evento("Aula de JavaScript", "Fullstack", link="https://auladejs.com.br/")
 
-eventos = [
-    aula_js,
-    aula_python,
-]
+# eventos = [
+#     aula_js,
+#     aula_python,
+# ]
--- agenda/views.py
+++ agenda/views.py
@@ -2,14 +2,20 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
 
-from agenda.models import eventos
 # Create your views here.
 def index(request):
     return HttpResponse("Olá mundo!")
 
 def exibir_evento(request):
-    evento = eventos[0]
+    evento = {
+        "nome": "Aula teste",
+        "categoria": "categoria teste",
+        "local": "ES",
+    }
     #template = loader.get_template("agenda/exibir_evento.html")
     #rendered_template = template.render(context={"evento": evento}, request=request) # injetar o contexto
     #return HttpResponse(rendered_template)
     return render(request=request, context={"evento": evento}, template_name="agenda/exibir_evento.html")
+
+
+
