f77c4130843ffc8da58213d1a7c3fee76b1c12bc
Danillo Teodoro <danillo.felicio2004@gmail.com>
2026-09-20 20:50:05 -0300

    configurando painel adm e db

--- agenda/models.py
+++ agenda/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
-
 class Categoria(models.Model): # mapea os atributos dessa classe para o db
     nome = models.CharField(max_length=256, unique=True)
+
+    def __str__(self): # metodo chamado ao tentar imprimir um obj dessa categoria
+        return f"{self.nome} <{self.id}>" # id vem da classe model
 
 # Create your models here.
 # classes que representam os modelos ou entidades da nossa aplicacao ex: usuarios
@@ -12,13 +14,16 @@ class Evento(models.Model):
     categoria = models.ForeignKey(Categoria, on_delete=models.SET_NULL, null=True) # é uma chave estrangeira para outro modelo
                                                                                    # caso seja deletado, setado como nulo
     local = models.CharField(max_length=256, blank=True)
-    link = models.CharField(max_length=256, blank=True)
+    link = models.CharField(max_length=256, blank=True) # blank quer dizer que o campo pode ficar em branco
 
     # def __init__(self, nome, categoria, local=None, link=None):
     #     self.nome = nome
     #     self.categoria = categoria
     #     self.local = local
     #     self.link = link
+
+    def __str__(self):
+        return f"{self.nome} <{self.id}>" 
         
 # aula_python = Evento("Aula de python", "backend", "Espirito santo")
 # aula_js = Evento("Aula de JavaScript", "Fullstack", link="https://auladejs.com.br/")
